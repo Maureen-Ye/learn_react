4a3f0efc646a0d52fe0d6f2f47d74221bfb27fb7
莫林 <807524935@qq.com>
2021-04-16 17:57:34 +0800

    多仓库合并

--- src/child.js
+++ src/child.js
@@ -3,7 +3,7 @@ import react from 'react';
 class Child extends react.Component{
   render() {
     return(
-        <div>{this.props.number}</div>
+        <div>{this.props.number}  </div>
     )
   }
 }
